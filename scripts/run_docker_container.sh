b0b11c361aec17596b40643203714e822971ae0d
armcortex <armcortexfpga@gmail.com>
2023-11-25 01:22:37 +0800

    update run_docker_container.sh with timezone binding

--- scripts/run_docker_container.sh
+++ scripts/run_docker_container.sh
@@ -1,5 +1,7 @@
 docker run \
   -it \
   --rm \
-  --mount type=bind,source="../$(pwd)",target=/app \
+  --mount type=bind,source="$(pwd)",target=/app \
+  --mount type=bind,source=/etc/localtime,target=/etc/localtime,readonly \
+  --mount type=bind,source=/etc/timezone,target=/etc/timezone,readonly \
   my-simple-db
